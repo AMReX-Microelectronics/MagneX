964a9b4075f255643f251f882b9feebbf2d222cc
jackieyao0114 <jackie_zhiyao@lbl.gov>
2022-08-29 14:11:32 -0700

    set up standard prob 4 for demag calculations

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,6 +3,8 @@
 #include <AMReX_ParmParse.H>
 #include <AMReX_MLABecLaplacian.H>
 #include <AMReX_MLMG.H> 
+#include <AMReX_OpenBC.H>
+#include <AMReX_MLPoisson.H>
 #include <AMReX_MultiFab.H> 
 #include <AMReX_VisMF.H>
 #include "myfunc.H"
@@ -49,16 +51,12 @@ void main_main ()
     amrex::GpuArray<amrex::Real, 3> mag_lo; // physical lo coordinate of magnetic region
     amrex::GpuArray<amrex::Real, 3> mag_hi; // physical hi coordinate of magnetic region
 
-    Real Phi_Bc_hi;
-    Real Phi_Bc_lo;
-
     int TimeIntegratorOrder;
 
     // Magnetic Properties
     Real alpha_val, gamma_val, Ms_val, exchange_val, anisotropy_val;
     Real mu0;
     amrex::GpuArray<amrex::Real, 3> anisotropy_axis; 
-
 
     int demag_coupling;
     int M_normalization;
@@ -83,9 +81,6 @@ void main_main ()
 
         // The domain is broken into boxes of size max_grid_size
         pp.get("max_grid_size",max_grid_size);
-
-        pp.get("Phi_Bc_hi",Phi_Bc_hi);
-        pp.get("Phi_Bc_lo",Phi_Bc_lo);
 
         pp.get("TimeIntegratorOrder",TimeIntegratorOrder);
 
@@ -174,7 +169,7 @@ void main_main ()
                      {AMREX_D_DECL( prob_hi[0], prob_hi[1], prob_hi[2])});
 
     // periodic in x and y directions
-    Array<int,AMREX_SPACEDIM> is_periodic{AMREX_D_DECL(1,1,0)};
+    Array<int,AMREX_SPACEDIM> is_periodic{AMREX_D_DECL(0,0,0)}; // nonperiodic in all directions
 
     // This defines a Geometry object
     geom.define(domain, real_box, CoordSys::cartesian, is_periodic);
@@ -205,10 +200,10 @@ void main_main ()
         Mfield_old[dir].define(ba, dm, Ncomp, Nghost);
     }
 
-    Array<MultiFab, AMREX_SPACEDIM> Hfield;
+    Array<MultiFab, AMREX_SPACEDIM> H_demagfield;
     for (int dir = 0; dir < AMREX_SPACEDIM; dir++)
     {
-        Hfield[dir].define(ba, dm, Ncomp, Nghost);
+        H_demagfield[dir].define(ba, dm, Ncomp, Nghost);
     }
 
     Array<MultiFab, AMREX_SPACEDIM> H_biasfield;
@@ -236,63 +231,15 @@ void main_main ()
     amrex::Print() << "=======================================================\n";
 
     MultiFab PoissonRHS(ba, dm, 1, 0);
-    MultiFab PoissonPhi(ba, dm, 1, 1);
+    MultiFab PoissonPhi(ba, dm, 1, 1); // one ghost cell
 
-    MultiFab Plt(ba, dm, 11, 0);
+    MultiFab Plt(ba, dm, 12, 0);
 
     //Solver for Poisson equation
     LPInfo info;
-    MLABecLaplacian mlabec({geom}, {ba}, {dm}, info);
-
-    //Force singular system to be solvable
-    mlabec.setEnforceSingularSolvable(false); 
-
-    // order of stencil
-    int linop_maxorder = 2;
-    mlabec.setMaxOrder(linop_maxorder);  
-
-    // build array of boundary conditions needed by MLABecLaplacian
-    std::array<LinOpBCType, AMREX_SPACEDIM> lo_mlmg_bc;
-    std::array<LinOpBCType, AMREX_SPACEDIM> hi_mlmg_bc; 
-
-    //Periodic 
-    for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-        if(is_periodic[idim]){
-          lo_mlmg_bc[idim] = hi_mlmg_bc[idim] = LinOpBCType::Periodic;
-        } else {
-          lo_mlmg_bc[idim] = hi_mlmg_bc[idim] = LinOpBCType::Dirichlet;
-        }
-    } 
-
-    mlabec.setDomainBC(lo_mlmg_bc,hi_mlmg_bc);
-
-    // coefficients for solver
-    MultiFab alpha_cc(ba, dm, 1, 0);
-    std::array< MultiFab, AMREX_SPACEDIM > beta_face;
-    AMREX_D_TERM(beta_face[0].define(convert(ba,IntVect(AMREX_D_DECL(1,0,0))), dm, 1, 0);,
-                 beta_face[1].define(convert(ba,IntVect(AMREX_D_DECL(0,1,0))), dm, 1, 0);,
-                 beta_face[2].define(convert(ba,IntVect(AMREX_D_DECL(0,0,1))), dm, 1, 0););
-    
-    // set cell-centered alpha coefficient to zero
-    alpha_cc.setVal(0.);
-    beta_face[0].setVal(1.);
-    beta_face[1].setVal(1.);
-    beta_face[2].setVal(1.);
-
-    // Set Dirichlet BC for Phi in z
-    SetPhiBC_z(PoissonPhi, n_cell, Phi_Bc_lo, Phi_Bc_hi); 
-    
-    // set Dirichlet BC by reading in the ghost cell values
-    mlabec.setLevelBC(0, &PoissonPhi);
-    
-    // (A*alpha_cc - B * div beta grad) phi = rhs
-    mlabec.setScalars(0.0, 1.0); // A = 0.0, B = 1.0
-    mlabec.setACoeffs(0, alpha_cc); //First argument 0 is lev
-    mlabec.setBCoeffs(0, amrex::GetArrOfConstPtrs(beta_face));  
-
-    //Declare MLMG object
-    MLMG mlmg(mlabec);
-    mlmg.setVerbose(2);
+    OpenBCSolver openbc({geom}, {ba}, {dm}, info);
+    openbc.setVerbose(2);
+    // openbc.setVerbose(2);
 
     // time = starting time in the simulation
     Real time = 0.0;	
@@ -320,81 +267,75 @@ void main_main ()
           Array4<Real> const &Hy_bias = H_biasfield[1].array(mfi);
           Array4<Real> const &Hz_bias = H_biasfield[2].array(mfi);
       
-          Array4<Real> const &Hx = Hfield[0].array(mfi);
-          Array4<Real> const &Hy = Hfield[1].array(mfi);
-          Array4<Real> const &Hz = Hfield[2].array(mfi);
+          Array4<Real> const &Hx_demag= H_demagfield[0].array(mfi);
+          Array4<Real> const &Hy_demag= H_demagfield[1].array(mfi);
+          Array4<Real> const &Hz_demag= H_demagfield[2].array(mfi);
               
           const Array4<Real>& Ms_arr = Ms.array(mfi);
 
+          amrex::Real angle_theta = 1.5650; // radiant of [111] direction
+          amrex::Real angle_phi = 0.2450; // radiant [111] direction
 
           amrex::ParallelFor( bx, [=] AMREX_GPU_DEVICE (int i, int j, int k)
           {
-             Hx_bias(i,j,k) = 0._rt;         
-             Hy_bias(i,j,k) = 3.7e4;
-             Hz_bias(i,j,k) = 0._rt;
-
-
              if (Ms_arr(i,j,k) > 0._rt)
              {
 
-//               Mx(i,j,k) = 1.4e5;
-//               My(i,j,k) = 0.;
-//               Mz(i,j,k) = 0.;
                 Real x = prob_lo[0] + (i+0.5) * dx[0];
                 Real y = prob_lo[1] + (j+0.5) * dx[1];
                 Real z = prob_lo[2] + (k+0.5) * dx[2];
                 
-                Mx(i,j,k) = (y < 0) ? 1.4e5 : 0.;
-                My(i,j,k) = 0._rt;
-                Mz(i,j,k) = (y >= 0) ? 1.4e5 : 0.;
+                // Mx(i,j,k) = (y < 0) ? Ms_arr(i,j,k) : 0.;
+                // My(i,j,k) = 0._rt;
+                // Mz(i,j,k) = (y >= 0) ? Ms_arr(i,j,k) : 0.;
+
+                Mx(i,j,k) = 1/sqrt(3.0)*Ms_arr(i,j,k);
+                My(i,j,k) = 1/sqrt(3.0)*Ms_arr(i,j,k);
+                Mz(i,j,k) = 1/sqrt(3.0)*Ms_arr(i,j,k);
+
+                // Mx(i,j,k) = 0._rt;
+                // My(i,j,k) = Ms_arr(i,j,k);
+                // Mz(i,j,k) = 0._rt;
+
              } else {
                 Mx(i,j,k) = 0.0;
                 My(i,j,k) = 0.0;
                 Mz(i,j,k) = 0.0;
-	     }
-
-             if(demag_coupling == 1)
-             { 
-//             //Solve Poisson's equation laplacian(Phi) = div(M) and get Hfield = -grad(Phi)
-//       	//Compute RHS of Poisson equation
-//      	ComputePoissonRHS(PoissonRHS, Mfield, 
-//      			mag_lo, mag_hi, 
-//      			prob_lo, prob_hi, 
-//      			geom);
-//
-//              //Initial guess for phi
-//              PoissonPhi.setVal(0.);
-//              mlmg.solve({&PoissonPhi}, {&PoissonRHS}, 1.e-10, -1);
-//      
-//              // Calculate H from Phi
-//      
-//      	ComputeHfromPhi(PoissonPhi, Hx, Hy, Hz, prob_lo, prob_hi, geom);
-//      
-               //Hx = ;
-               //Hy = ;
-               //Hz = ;
-             }
+            }
           });
 
     } 
+    if(demag_coupling == 1){ 
+        //Solve Poisson's equation laplacian(Phi) = div(M) and get H_demagfield = -grad(Phi)
+        //Compute RHS of Poisson equation
+        ComputePoissonRHS(PoissonRHS, Mfield, Ms, geom);
+        
+        //Initial guess for phi
+        PoissonPhi.setVal(0.);
+        openbc.solve({&PoissonPhi}, {&PoissonRHS}, 1.e-10, -1); 
+
+        // Calculate H from Phi
+        ComputeHfromPhi(PoissonPhi, H_demagfield, prob_lo, prob_hi, geom);
+    }
  
     // Write a plotfile of the initial data if plot_int > 0
     if (plot_int > 0)
     {
         int step = 0;
         const std::string& pltfile = amrex::Concatenate("plt",step,8);
-        MultiFab::Copy(Plt, alpha, 0, 0, 1, 0);  
+        MultiFab::Copy(Plt, PoissonRHS, 0, 0, 1, 0);  
         MultiFab::Copy(Plt, Ms, 0, 1, 1, 0);
-        MultiFab::Copy(Plt, gamma, 0, 2, 1, 0);
-        MultiFab::Copy(Plt, exchange, 0, 3, 1, 0);
-        MultiFab::Copy(Plt, anisotropy, 0, 4, 1, 0);
+        MultiFab::Copy(Plt, H_demagfield[0], 0, 2, 1, 0);
+        MultiFab::Copy(Plt, H_demagfield[1], 0, 3, 1, 0);
+        MultiFab::Copy(Plt, H_demagfield[2], 0, 4, 1, 0);
         MultiFab::Copy(Plt, Mfield[0], 0, 5, 1, 0);
         MultiFab::Copy(Plt, Mfield[1], 0, 6, 1, 0);
         MultiFab::Copy(Plt, Mfield[2], 0, 7, 1, 0);
         MultiFab::Copy(Plt, H_biasfield[0], 0, 8, 1, 0);
         MultiFab::Copy(Plt, H_biasfield[1], 0, 9, 1, 0);
         MultiFab::Copy(Plt, H_biasfield[2], 0, 10, 1, 0);
-        WriteSingleLevelPlotfile(pltfile, Plt, {"alpha","Ms","gamma","exchange","anisotropy","Mx", "My", "Mz", "Hx_bias", "Hy_bias", "Hz_bias"}, geom, time, 0);
+        MultiFab::Copy(Plt, PoissonPhi, 0, 11, 1, 0);
+        WriteSingleLevelPlotfile(pltfile, Plt, {"PoissonRHS","Ms","Hx_demag","Hy_demag","Hz_demag","Mx", "My", "Mz", "Hx_bias", "Hy_bias", "Hz_bias", "PoissonPhi"}, geom, time, 0);
     }
 
     for (int step = 1; step <= nsteps; ++step)
@@ -411,8 +352,20 @@ void main_main ()
 
         Real step_strt_time = ParallelDescriptor::second();
 
-    	    // Evolve M
-
+    	// Evolve M
+        if(demag_coupling == 1)
+        {
+            //Solve Poisson's equation laplacian(Phi) = div(M) and get H_demagfield = -grad(Phi)
+            //Compute RHS of Poisson equation
+            ComputePoissonRHS(PoissonRHS, Mfield_old, Ms, geom);
+            
+            //Initial guess for phi
+            PoissonPhi.setVal(0.);
+            openbc.solve({&PoissonPhi}, {&PoissonRHS}, 1.e-10, -1);
+    
+            // Calculate H from Phi
+            ComputeHfromPhi(PoissonPhi, H_demagfield, prob_lo, prob_hi, geom);
+        }
 
         //for (MFIter mfi(*Mfield[0]); mfi.isValid(); ++mfi)
         for (MFIter mfi(Mfield[0]); mfi.isValid(); ++mfi)
@@ -421,9 +374,9 @@ void main_main ()
               const Box& bx = mfi.growntilebox(1); 
 
         // extract field data
-              Array4<Real> const &Hx = Hfield[0].array(mfi);
-              Array4<Real> const &Hy = Hfield[1].array(mfi);
-              Array4<Real> const &Hz = Hfield[2].array(mfi);
+              Array4<Real> const &Hx_demag= H_demagfield[0].array(mfi);
+              Array4<Real> const &Hy_demag= H_demagfield[1].array(mfi);
+              Array4<Real> const &Hz_demag= H_demagfield[2].array(mfi);
               Array4<Real> const &Mx = Mfield[0].array(mfi);         
               Array4<Real> const &My = Mfield[1].array(mfi);         
               Array4<Real> const &Mz = Mfield[2].array(mfi);         
@@ -439,20 +392,58 @@ void main_main ()
               const Array4<Real>& Ms_arr = Ms.array(mfi);
               const Array4<Real>& exchange_arr = exchange.array(mfi);
               const Array4<Real>& anisotropy_arr = anisotropy.array(mfi);
+
+              amrex::Real t0 = 1.0e-9; // time when bias reduces to zero
+              amrex::Real frequency = 2.e9;
+              amrex::Real TP = 1/frequency;
+              amrex::Real pi = 3.14159265358979;
  
-              amrex::ParallelFor( bx, [=] AMREX_GPU_DEVICE (int i, int j, int k)
+              amrex::ParallelForRNG( bx, [=] AMREX_GPU_DEVICE (int i, int j, int k, amrex::RandomEngine const& engine) noexcept
               {
                  if (Ms_arr(i,j,k) > 0._rt)
                  {
+
+                    if (time <= t0){
+                        Hx_bias(i,j,k) = 1.0e5 * (1-time/t0) * 1._rt/sqrt(3);
+                        Hy_bias(i,j,k) = 1.0e5 * (1-time/t0) * 1._rt/sqrt(3);
+                        Hz_bias(i,j,k) = 1.0e5 * (1-time/t0) * 1._rt/sqrt(3);
+                    } else {
+                        Hx_bias(i,j,k) = 0.0 ;
+                        Hy_bias(i,j,k) = 0.0 ;
+                        Hz_bias(i,j,k) = 0.0 ;
+                    }
+
+                    // amrex::Real z = prob_lo[2] + (k+0.5) * dx[2];
+                    
+                    // Hx_bias(i,j,k) = 24.0 * (exp(-(time-3.* TP)*(time-3.* TP)/(2*TP*TP))*cos(2*pi*frequency*time)) * cos(z / 345.0e-9 * pi);
+                    // Hy_bias(i,j,k) = 2.4e4;
+                    // Hz_bias(i,j,k) = 0.;
+
+                    // Hx_bias(i,j,k) = (j < 32) ? 1.0e5 : 0.;
+                    // Hy_bias(i,j,k) = (j < 32) ? 0. : 1.0e5;
+                    // Hz_bias(i,j,k) = 0.;
+
                     amrex::Real Hx_eff = Hx_bias(i,j,k);
                     amrex::Real Hy_eff = Hy_bias(i,j,k);
                     amrex::Real Hz_eff = Hz_bias(i,j,k);
-                 
+
+                    Hx_eff += (-1.0 + 2.0*Random(engine))*0.002; // add random noise
+                    Hy_eff += (-1.0 + 2.0*Random(engine))*0.002; // add random noise
+                    Hz_eff += (-1.0 + 2.0*Random(engine))*0.002; // add random noise
+                    
                     if(demag_coupling == 1)
                     {
-                      Hx_eff += Hx(i,j,k);
-                      Hy_eff += Hy(i,j,k);
-                      Hz_eff += Hz(i,j,k);
+                        Hx_eff += Hx_demag(i,j,k);
+                        Hy_eff += Hy_demag(i,j,k);
+                        Hz_eff += Hz_demag(i,j,k);
+
+                        if (i == 128 && j == 32 && k == 4){
+                            printf("got here \n");
+                            printf("Hx_demag = %g", Hx_demag(i,j,k));
+                            printf("Hy_demag = %g", Hy_demag(i,j,k));
+                            printf("Hz_demag = %g", Hz_demag(i,j,k));
+                        }
+                        
                     }
 
                     if(exchange_coupling == 1)
@@ -470,7 +461,7 @@ void main_main ()
                       amrex::Real Ms_lo_z = Ms_arr(i, j, k-1);
                       amrex::Real Ms_hi_z = Ms_arr(i, j, k+1);
 
-		      if(i == 31 && j == 31 && k == 31) printf("Laplacian_x = %g \n", Laplacian_Mag(Mx_old, Ms_lo_x, Ms_hi_x, Ms_lo_y, Ms_hi_y, Ms_lo_z, Ms_hi_z, i, j, k, dx));
+                      if(i == 31 && j == 31 && k == 31) printf("Laplacian_x = %g \n", Laplacian_Mag(Mx_old, Ms_lo_x, Ms_hi_x, Ms_lo_y, Ms_hi_y, Ms_lo_z, Ms_hi_z, i, j, k, dx));
                       Hx_eff += H_exchange_coeff * Laplacian_Mag(Mx_old, Ms_lo_x, Ms_hi_x, Ms_lo_y, Ms_hi_y, Ms_lo_z, Ms_hi_z, i, j, k, dx);
                       Hy_eff += H_exchange_coeff * Laplacian_Mag(My_old, Ms_lo_x, Ms_hi_x, Ms_lo_y, Ms_hi_y, Ms_lo_z, Ms_hi_z, i, j, k, dx);
                       Hz_eff += H_exchange_coeff * Laplacian_Mag(Mz_old, Ms_lo_x, Ms_hi_x, Ms_lo_y, Ms_hi_y, Ms_lo_z, Ms_hi_z, i, j, k, dx);
@@ -531,6 +522,7 @@ void main_main ()
                        if (amrex::Math::abs(1._rt - M_magnitude_normalized) > normalized_error)
                        {
                            printf("M_magnitude_normalized = %g \n", M_magnitude_normalized);
+                           printf("i = %d, j = %d, k = %d \n", i, j,k);
                            amrex::Abort("Exceed the normalized error of the Mx field");
                        }
                        // normalize the M field
@@ -560,32 +552,6 @@ void main_main ()
               });     
                       
      }  
-	
-//        // copy new solution into old solution
-//        for(int comp = 0; comp < 3; comp++)
-//        {
-//           MultiFab::Copy(Mfield_old[comp], Mfield[comp], 0, 0, 1, 1);
-//
-//           // fill periodic ghost cells
-//           Mfield_old[comp].FillBoundary(geom.periodicity());
-//
-//        }
-
-//             //Solve Poisson's equation laplacian(Phi) = div(M) and get Hfield = -grad(Phi)
-//       	//Compute RHS of Poisson equation
-//      	ComputePoissonRHS(PoissonRHS, Mfield, 
-//      			mag_lo, mag_hi, 
-//      			prob_lo, prob_hi, 
-//      			geom);
-//
-//              //Initial guess for phi
-//              PoissonPhi.setVal(0.);
-//              mlmg.solve({&PoissonPhi}, {&PoissonRHS}, 1.e-10, -1);
-//      
-//              // Calculate H from Phi
-//      
-//      	ComputeHfromPhi(PoissonPhi, Hx, Hy, Hz, prob_lo, prob_hi, geom);
-//      
 
 	Real step_stop_time = ParallelDescriptor::second() - step_strt_time;
         ParallelDescriptor::ReduceRealMax(step_stop_time);
@@ -598,18 +564,19 @@ void main_main ()
         if (plot_int > 0 && step%plot_int == 0)
         {
             const std::string& pltfile = amrex::Concatenate("plt",step,8);
-            MultiFab::Copy(Plt, alpha, 0, 0, 1, 0);  
+            MultiFab::Copy(Plt, PoissonRHS, 0, 0, 1, 0);  
             MultiFab::Copy(Plt, Ms, 0, 1, 1, 0);
-            MultiFab::Copy(Plt, gamma, 0, 2, 1, 0);
-            MultiFab::Copy(Plt, exchange, 0, 3, 1, 0);
-            MultiFab::Copy(Plt, anisotropy, 0, 4, 1, 0);
+            MultiFab::Copy(Plt, H_demagfield[0], 0, 2, 1, 0);
+            MultiFab::Copy(Plt, H_demagfield[1], 0, 3, 1, 0);
+            MultiFab::Copy(Plt, H_demagfield[2], 0, 4, 1, 0);
             MultiFab::Copy(Plt, Mfield[0], 0, 5, 1, 0);
             MultiFab::Copy(Plt, Mfield[1], 0, 6, 1, 0);
             MultiFab::Copy(Plt, Mfield[2], 0, 7, 1, 0);
             MultiFab::Copy(Plt, H_biasfield[0], 0, 8, 1, 0);
             MultiFab::Copy(Plt, H_biasfield[1], 0, 9, 1, 0);
             MultiFab::Copy(Plt, H_biasfield[2], 0, 10, 1, 0);
-            WriteSingleLevelPlotfile(pltfile, Plt, {"alpha","Ms","gamma","exchange","anisotropy","Mx", "My", "Mz", "Hx_bias", "Hy_bias", "Hz_bias"}, geom, time, step);
+            MultiFab::Copy(Plt, PoissonPhi, 0, 11, 1, 0);
+            WriteSingleLevelPlotfile(pltfile, Plt, {"PoissonRHS","Ms","Hx_demag","Hy_demag","Hz_demag","Mx", "My", "Mz", "Hx_bias", "Hy_bias", "Hz_bias", "PoissonPhi"}, geom, time, step);
         }
 
         // MultiFab memory usage
