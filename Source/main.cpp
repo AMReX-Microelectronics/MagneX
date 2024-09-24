bbdc658f209d828edd3dca25ee0807777cac1e4a
Andy Nonaka <AJNonaka@lbl.gov>
2024-09-24 15:06:49 -0700

    dmi with demag requires dx=dy=dz

--- Source/main.cpp
+++ Source/main.cpp
@@ -209,6 +209,17 @@ void main_main ()
     Demagnetization demag_solver;
     
     if (demag_coupling == 1) {
+
+        const Real* dx = geom.CellSize();
+        if (dx[0] != dx[1]) {
+            Abort("Demag requires dx=dy");
+        }
+#if (AMREX_SPACEDIM==3)
+        if (dx[0] != dx[2]) {
+            Abort("Demag requires dx=dy=dz");
+        }
+#endif
+
         demag_solver.define();       
     }
 
